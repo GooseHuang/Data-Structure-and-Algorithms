9a34389c68342a9770bb289f61bc7dc311aea6fd
Qiliang Huang <goosehuangmatt@gmail.com>
2023-02-08 20:11:38 +0800

    Update Threading

--- Threading/Threading.py
+++ Threading/Threading.py
@@ -1,36 +1,31 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
- 
 import threading
 import time
- 
+
 exitFlag = 0
- 
-class myThread (threading.Thread):   #继承父类threading.Thread
-    def __init__(self, threadID, name, counter):
+
+def main():
+    time.sleep(1)
+
+
+class myThread(threading.Thread):  # 继承父类threading.Thread
+    def __init__(self, threadID, ):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.name = name
-        self.counter = counter
-    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
-        print("Starting " + self.name)
-        print_time(self.name, self.counter, 5)
-        print("Exiting " + self.name)
 
-def print_time(threadName, delay, counter):
-    while counter:
-        if exitFlag:
-            (threading.Thread).exit()
-        time.sleep(delay)
-        print("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
+    def run(self):  # 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
+        print(f"Starting thread: {str(self.threadID)}")
+
+        main()
+
+        print(f"Exiting threadL {str(self.threadID)}")
+
 
 # 创建新线程
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
- 
+thread1 = myThread(1)
+thread2 = myThread(2)
+thread3 = myThread(3)
 # 开启线程
 thread1.start()
 thread2.start()
- 
-print("Exiting Main Thread")
+thread3.start()
+print("Exiting Main Thread\n")
